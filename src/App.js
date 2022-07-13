55ad96de2a103d28ec3807619b79cb09fbb479f2
SR0sohan <srssohan360@gmail.com>
2022-07-13 21:09:19 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>      
+      <Navbar Title="TextUtils" About="About TextUtils"/>
+    </div>
+  );
+}
+
+export default App;
