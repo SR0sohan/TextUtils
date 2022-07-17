6134a6e26cabcc1e6fc2577d75bdbc871c314c54
SR0sohan <srssohan360@gmail.com>
2022-07-17 11:25:21 +0600

    newF

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,18 @@ export default function TextForm(props) {
         setText(newText)
     
     }
+    const handlelow = () =>{
+      let newText = text.toLowerCase();
+      setText(newText)
+    }
+    const handlclear =() =>{
+      let newText= '';
+      setText(newText)
+    }
+    const handlReverse =() =>{
+      let newText = text.split("").reverse().join("");
+      setText(newText)
+    }
 
     const handleChange = (event) =>{
         // console.log('change is happening');
@@ -15,12 +27,24 @@ export default function TextForm(props) {
 
     const [text, setText] =useState('Enter text here')
   return (
-    <div>
-        <h1 className="mt-5">{props.heading}</h1>
-        <div className="mt-4">
-        <textarea className="form-control" value={text} onChange={handleChange} id="Textarea" rows="8"></textarea>
-        </div>
-        <button type="button" onClick={handleUp} className="btn btn-success mt-3">Convert To Uppercase</button>
-    </div>
+    <>
+      <div className='container'>
+          <h1 className="mt-5">{props.heading}</h1>
+          <div className="mt-4">
+          <textarea className="form-control" value={text} onChange={handleChange} id="Textarea" rows="8"></textarea>
+          </div>
+          <button type="button" onClick={handleUp} className="btn btn-success mt-3">Convert To Uppercase</button>
+          <button type="button" onClick={handlelow} className="btn btn-success mt-3 ms-3">Convert To Lowercase</button>
+          <button type="button" onClick={handlReverse} className="btn btn-success mt-3 ms-3">Reverse your text</button>
+          <button type="button" onClick={handlclear} className="btn btn-success mt-3 ms-3">Clear</button>
+      </div>
+      <div className="dontainer my-3">
+          <h2>Preview</h2>
+          <p>{text.split(" ").length} words and {text.length} characters</p>
+          <p>Approximately {0.08 * text.split(" ").length} muinutes to read.</p>
+          <h3>Summary</h3>
+          <p>{text}</p>
+      </div>
+    </>
   )
 }
